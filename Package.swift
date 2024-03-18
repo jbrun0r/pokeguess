0907c223b3bac701891368a84da800fffb2ea5e2
João Bruno <joaobruno.rf@gmail.com>
2024-03-18 10:50:54 -0300

    First commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "pokeguess",
+    dependencies: [
+            .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        ],
+    
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "pokeguess",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]),
+    ]
+)
---
+++ Sources/main.swift
@@ -0,0 +1,83 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ArgumentParser
+
+struct Pokeguess: ParsableCommand {
+    
+    static let configuration = CommandConfiguration(
+        abstract: "Pokémon Guesser",
+        discussion: """
+        A guessing game of first-generation Pokémon. Enter the name of a Pokémon and
+        receive a comparison of the key features between the entered Pokémon and the
+        target Pokémon. Try to guess with the fewest number of attempts.
+        """,
+        subcommands: [Guess.self, Hint.self, Info.self, List.self])
+    
+    mutating func run() throws {
+        print("""
+            Pokegues run...
+            """)
+            
+    }
+}
+
+extension Pokeguess {
+    
+    struct Guess: ParsableCommand {
+        static var configuration = CommandConfiguration(
+        abstract: "Try to guess the target Pokémon",
+        discussion: """
+                    Enter the name of a Pokémon and receive a comparison of the key features
+                    between the entered Pokémon and the target Pokémon.
+                    """)
+        
+        @Argument(help: "Pokémon name") var pokemon: String
+
+
+        mutating func run() {
+            print("Info: \(pokemon)")
+        }
+    }
+    
+    struct Hint: ParsableCommand {
+        static var configuration = CommandConfiguration(
+        abstract: "Shows a hint about the target pokemon",
+        discussion: """
+                   This subcommand is used to display an extra feature of the target Pokémon.
+                   """)
+
+        mutating func run() {
+            print("hint: ")
+        }
+    }
+    
+    struct Info: ParsableCommand {
+        static var configuration = CommandConfiguration(
+        abstract: "Shows information about available pokemon",
+        discussion: """
+                   This subcommand display the features of a specific Pokémon.
+                   """)
+        
+        @Argument(help: "Pokémon name") var pokemon: String
+
+
+        mutating func run() {
+            print("Info: \(pokemon)")
+        }
+    }
+    
+    struct List: ParsableCommand {
+        static var configuration = CommandConfiguration(
+        abstract: "List all available pokemon",
+        discussion: """
+                   This subcommand lists all available Pokémon.
+                   """)
+
+        mutating func run() {
+            print("List: ")
+        }
+    }
+}
+
+Pokeguess.main()

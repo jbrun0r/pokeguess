8159670c7ea005710cb8883d754f3be1aed3b126
Victor Hugo <victorhugoaraujo8711@gmail.com>
2024-03-21 10:58:38 -0300

    feat: subcommand guess

--- Sources/Models/Pokemon.swift
+++ Sources/Models/Pokemon.swift
@@ -1,17 +1,15 @@
 import Foundation
 
-struct Pokemon: Codable  {
+struct Pokemon: Codable {
     let id: Int
     let name: String
     let types: [String]
     let abilities: [String]
-    let primary_color: String
+    let primaryColor: String
     let habitat: String
-    let evolution_stage: Int
+    let evolutionStage: Int
     let height: Double
     let weight: Double
     let hint: String
-    
-    
     let sprite: String
 }
--- Sources/Services/PokemonService.swift
+++ Sources/Services/PokemonService.swift
@@ -10,27 +10,26 @@ func comparePokemons(_ entered: Pokemon, _ target: Pokemon) -> (String, Bool) {
         concatenatedAtt = formatString(entered.name, Color.green)
     }
     
-    
     for i in 0...1 {
-        if entered.types[i] == target.types[0] || entered.types[i] == target.types[1]{
+        if entered.types[i] == target.types[0] || entered.types[i] == target.types[1] {
             concatenatedAtt += formatString(entered.types[i], Color.green)
-        } else{
+        } else {
             concatenatedAtt += formatString(entered.types[i], Color.red)
         }
     }
     
     for i in 0...1 {
-        if entered.abilities[i] == target.abilities[0] || entered.abilities[i] == target.abilities[1]{
+        if entered.abilities[i] == target.abilities[0] || entered.abilities[i] == target.abilities[1] {
             concatenatedAtt += formatString(entered.abilities[i], Color.green)
-        } else{
+        } else {
             concatenatedAtt += formatString(entered.abilities[i], Color.red)
         }
     }
     
-    if entered.primary_color == target.primary_color {
-        concatenatedAtt += formatString(entered.primary_color, Color.green)
+    if entered.primaryColor == target.primaryColor {
+        concatenatedAtt += formatString(entered.primaryColor, Color.green)
     } else {
-        concatenatedAtt += formatString(entered.primary_color, Color.red)
+        concatenatedAtt += formatString(entered.primaryColor, Color.red)
     }
     
     if entered.habitat == target.habitat {
@@ -39,15 +38,16 @@ func comparePokemons(_ entered: Pokemon, _ target: Pokemon) -> (String, Bool) {
         concatenatedAtt += formatString(entered.habitat, Color.red)
     }
     
-    if entered.evolution_stage == target.evolution_stage {
-        concatenatedAtt += formatString("\(entered.evolution_stage)", Color.green)
-    } else if entered.evolution_stage < target.evolution_stage {
-        concatenatedAtt += formatString("\(entered.evolution_stage) ⬆", Color.red)
+    if entered.evolutionStage == target.evolutionStage {
+        concatenatedAtt += formatString("\(entered.evolutionStage)", Color.green)
+    } else if entered.evolutionStage < target.evolutionStage {
+        concatenatedAtt += formatString("\(entered.evolutionStage) ⬆", Color.red)
     } else {
-        concatenatedAtt += formatString("\(entered.evolution_stage) ⬇", Color.red)
+        concatenatedAtt += formatString("\(entered.evolutionStage) ⬇", Color.red)
     }
     
     let entereHeight: String = String(format: "%.1f", entered.height / 10)
+    
     if entered.height == target.height {
         concatenatedAtt += formatString(entereHeight, Color.green)
     } else if entered.height < target.height {
@@ -57,6 +57,7 @@ func comparePokemons(_ entered: Pokemon, _ target: Pokemon) -> (String, Bool) {
     }
     
     let enteredWeight: String = String(format: "%.1f", entered.weight / 10)
+    
     if entered.weight == target.weight {
         concatenatedAtt += formatString(enteredWeight, Color.green)
     } else if entered.weight < target.weight {
@@ -65,7 +66,14 @@ func comparePokemons(_ entered: Pokemon, _ target: Pokemon) -> (String, Bool) {
         concatenatedAtt += formatString(enteredWeight + "kg ⬇", Color.red)
     }
     
-    
     return (concatenatedAtt, isEqual)
 }
 
+func sortNewPokemon(_ lenght: Int) {
+    do {
+        let randomId = Int.random(in: 1...lenght)
+        try writeJSON(randomId, to: "target.json")
+    } catch {
+        print("Sort new pokémon error: \(error)")
+    }
+}

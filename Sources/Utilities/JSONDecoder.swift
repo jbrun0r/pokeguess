2a2f21fc79eead1a52b68bede97d30bec59fd4c9
Victor Hugo <victorhugoaraujo8711@gmail.com>
2024-03-21 09:02:03 -0300

    feat: write and read JSON

---
+++ Sources/Utilities/JSONDecoder.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func readJSON<T: Decodable>(type: T.Type,  fileName: String) throws -> T {
+    
+    let scriptURL = URL(fileURLWithPath: #file)
+    let currentDirectory = scriptURL.deletingLastPathComponent()
+    let jsonFilePath = currentDirectory.appendingPathComponent(fileName).path
+
+    guard let jsonData = try? Data(contentsOf: URL(fileURLWithPath: jsonFilePath)) else {
+        throw NSError(domain: "JSONDecodeError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Não foi possível ler o arquivo JSON."])
+    }
+
+    let decoder = JSONDecoder()
+    return try decoder.decode(type, from: jsonData)
+}
